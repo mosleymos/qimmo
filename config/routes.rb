ff62224cf7f75d742bd31ba5264bf0ea1cc988e5
André Baudin-Laurencin <abaudin@gmail.com>
2014-02-05 13:51:16 +0100

    Resource bienvenue rajoutée

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Qimmo::Application.routes.draw do
+
+
+
+  resources :bienvenues
+
   resources :utilisateurs
 
   resources :programmes
@@ -9,7 +14,7 @@ Qimmo::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
